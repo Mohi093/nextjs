e4eb12e2e38c28a6216b285d80fdec443a98478d
Mohi093 <150961890+Mohi093@users.noreply.github.com>
2025-01-20 21:21:39 +0100

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,6 @@
 import { useEffect } from 'react';
 import Head from 'next/head';
-import styles from '@/styles/Home.module.css';
+import styles from '../styles/Home.module.css';
 
 export default function Home() {
   useEffect(() => {
